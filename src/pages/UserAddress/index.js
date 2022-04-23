48d8e8e10784bd357ef5c4e0554d9969ca5a000a
Herlambang Kuswicaksonojati <herlambangk25@gmail.com>
2022-04-24 05:58:46 +0700

    Tabel alamat pengiriman

--- src/pages/UserAddress/index.js
+++ src/pages/UserAddress/index.js
@@ -6,8 +6,58 @@ import { LayoutOne, Text, Table, Button } from "upkit";
 import { Link } from "react-router-dom";
 
 export default function UserAddress() {
+  const columns = [
+    { Header: "Nama", accessor: "nama" },
+    {
+      Header: "Detail",
+      accessor: (alamat) => {
+        return (
+          <div>
+            {alamat.provinsi}, {alamat.kabupaten} ,{alamat.kecamatan},
+            {alamat.kelurahan} <br />
+            {alamat.detail}
+          </div>
+        );
+      },
+    },
+  ];
+
   let { data, limit, page, status, count, setPage } = useAddressData();
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <LayoutOne size="large">
+      <div>
+        <TopBar />
+        <Text as="h3"> Alamat pengiriman </Text>
+        <br />
+
+        <div>
+          <Link to="alamat-pengiriman/tambah">
+            <Button>Tambah baru</Button>
+          </Link>
+          <br />
+          <br />
+          <Table
+            items={data}
+            columns={columns}
+            totalItems={count}
+            page={page}
+            perPage={limit}
+            isLoading={status === "process"}
+            onPageChange={(page) => setPage(page)}
+          />
+        </div>
+
+        {status === "success" && !data.length ? (
+          <div className="text-center p-10">
+            Kamu belum menambahkan alamat pengiriman. <br />
+            <Link to="/alamat-pengiriman/tambah">
+              <Button> Tambah Baru </Button>
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </LayoutOne>
+  );
 }
 
 //   return <div>{JSON.stringify(data)}</div>;
